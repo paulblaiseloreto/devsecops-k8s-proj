a4415e4c565c49557692e08a14c74c39acdc8fba
Paul <paul.loreto@joist.com>
2022-10-05 18:17:09 -0400

    Add test steps at the Jenkinsfile

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -19,7 +19,7 @@ public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private static final String baseURL = "http://node-service:5000/plusone";
-	
+
 	RestTemplate restTemplate = new RestTemplate();
 	
 	@RestController
